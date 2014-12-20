b4794e59c5acfb3d9103bc8732295adb47643346
Laurent Bercot <ska-skaware@skarnet.org>
2014-12-20 00:09:01 +0000

    Test a shutdown in s6-sudoc, see if it helps the BSDs

--- src/conn-tools/s6-sudoc.c
+++ src/conn-tools/s6-sudoc.c
@@ -1,5 +1,7 @@
 /* ISC license. */
 
+#include <skalibs/nonposix.h>
+#include <sys/socket.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
@@ -91,6 +93,7 @@ int main (int argc, char const *const *argv, char const *const *envp)
   if (!unixmessage_sender_timed_flush_g(&b7, &deadline))
     strerr_diefu1sys(111, "send args to server") ;
   unixmessage_sender_free(&b7) ;
+  shutdown(7, SHUT_WR) ;
   {
     char c ;
     if (buffer_timed_get_g(&b6, &c, 1, &deadline) < 1)
