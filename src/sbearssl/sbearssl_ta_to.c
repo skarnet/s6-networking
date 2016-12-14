399086d947bc2999fa2d93f47a08c5ccba024197
Laurent Bercot <ska-skaware@skarnet.org>
2016-12-14 10:33:27 +0000

    sbearssl: follow API change for br_x590_trust_anchor

--- src/sbearssl/sbearssl_ta_to.c
+++ src/sbearssl/sbearssl_ta_to.c
@@ -5,8 +5,8 @@
 
 void sbearssl_ta_to (sbearssl_ta const *sta, br_x509_trust_anchor *bta, char *s)
 {
-  bta->dn = (unsigned char *)s + sta->dn ;
-  bta->dn_len = sta->dnlen ;
+  bta->dn.data = (unsigned char *)s + sta->dn ;
+  bta->dn.len = sta->dnlen ;
   bta->flags = sta->flags ;
   sbearssl_pkey_to(&sta->pkey, &bta->pkey, s) ;
 }
